e7afcc37d33b5af81fb3d2ed6c7a057592ba6e8c
JJ MM <jjmm110203@gmail.com>
2022-07-26 06:42:46 -0400

    Textures ex.3

---
+++ 4_Textures/Exercises/3/shader.fs
@@ -0,0 +1,22 @@
+#version 460 core
+out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
+// texture sampler
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main()
+{
+	/*
+		Mix both textures, the 0.2 says that the resulting
+		texture will have 80% (0.8) of the first texture
+		and 20% (0.2) of the second texture
+	*/
+
+	FragColor = mix(texture(texture1, TexCoord),
+					texture(texture2, TexCoord), 0.2);
+}
+
